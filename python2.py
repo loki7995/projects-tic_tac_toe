a77220f9825c4451d1f39a22c55bc029304b2962
Lokanath <lokeshlokanath9@gmail.com>
2026-01-03 21:32:11 +0530

    Update python2.py logic

--- python2.py
+++ python2.py
@@ -1,1 +1,14 @@
 print("this is loki")
+class loki:
+    def loop(i):
+        for i in range(1, 100):
+            print(i)
+            i += 1
+            print("complted iter")
+
+
+e = loki()
+print(e.loop(1))
+
+
+
